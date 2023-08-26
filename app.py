40c98ad6c1eb0b055855712f5781b71f494e5cac
Himanshu Bhutani <bhutanihimanshu98@gmail.com>
2023-08-27 02:58:15 +0530

    Update code

--- app.py
+++ app.py
@@ -17,19 +17,28 @@ weekday_mapping = {
     "sunday": 6
 }
 
-def parse_date_range(input_text, keyword):
+def remove_words(input_string, words_to_remove):
+    cleaned_string = input_string
+    for word in words_to_remove:
+        cleaned_string = cleaned_string.replace(word, "")
+    return cleaned_string
+
+
+def parse_date_range(input_text, keyword,doc):
     # Define a pattern to match the "from [start date] to [end date]" format
     date_range_pattern = r"from (.+) to (.+)"
     
     # Search for date ranges using the pattern
-    match = re.search(date_range_pattern, input_text)
+    input_text =  remove_words(input_text,["next","upcoming"])
     
+    match = re.search(date_range_pattern, input_text)
+
     if match:
         start_date_str = match.group(1)
         end_date_str = match.group(2)
         
-        start_date = dateparser.parse(start_date_str)
-        end_date = dateparser.parse(end_date_str)
+        start_date = dateparser.parse(start_date_str,settings={'PREFER_DATES_FROM': 'future'})
+        end_date = dateparser.parse(end_date_str,settings={'PREFER_DATES_FROM': 'future'})
         
         if start_date and end_date:
             response = f"Okay, your {keyword} is scheduled from {start_date.strftime('%d %B')} to {end_date.strftime('%d %B')}."
@@ -49,28 +58,8 @@ def parse_request(input_text):
     keyword = "meeting" if is_meeting_request else "out-of-office"
     
     # Check for different patterns based on cases
-    response = ""
-    if "from" in input_text and "to" in input_text:
-        response = parse_date_range(input_text, keyword)
-    elif "next" in input_text and any(token.text in weekday_mapping for token in doc):
-        # Handle cases like "next Monday to Tuesday"
-        current_date = datetime.now()
-        
-        tokens = [token for token in doc if token.text in weekday_mapping]
-        start_weekday, end_weekday = tokens[0].text, tokens[1].text
-        
-        start_days_until_weekday = (weekday_mapping[start_weekday] - current_date.weekday()) % 7
-        end_days_until_weekday = (weekday_mapping[end_weekday] - current_date.weekday()) % 7
-        
-        # Special case handling when transitioning from end of week to start of next week
-        if start_days_until_weekday <= end_days_until_weekday:
-            start_date = current_date + timedelta(days=start_days_until_weekday)
-            end_date = current_date + timedelta(days=end_days_until_weekday)
-        else:
-            start_date = current_date + timedelta(days=start_days_until_weekday)
-            end_date = current_date + timedelta(days=end_days_until_weekday + 7)
-        
-        response = f"Okay, your {keyword} is scheduled from {start_date.strftime('%d %B')} to {end_date.strftime('%d %B')}."
+    response = parse_date_range(input_text, keyword,doc)
+
     
     # Handle other cases here if needed
     
@@ -81,32 +70,22 @@ def parse_request(input_text):
 
 # Test cases
 
-input_text = "Schedule ooo from 1 June to 5 June"
-response = parse_request(input_text)
-print(response)
+test_cases = [
+    "Schedule ooo from today to next monday",
+    "Schedule my out of office from 1 June to tuesday",
+    "Schedule meeting from 1 June to 5 June",
+    "Schedule a meeting from 1 June to tuesday",
+    "Schedule meeting from today to tomorrow",
+    "Schedule meeting from yesterday to tomorrow",
+    "Schedule meeting from monday to tuesday",
+    "Schedule meeting from monday to tuesday",
+    "Schedule meeting from monday to wednesday",
+    "Schedule meeting from 25 to monday",
+    "Schedule meeting from next monday to next tuesday",
+]
 
-input_text = "Schedule my out of office from 1 June to tuesday"
-response = parse_request(input_text)
-print(response)
-
-input_text = "Schedule meeting from 1 June to 5 June"
-response = parse_request(input_text)
-print(response)
-
-input_text = "Schedule a meeting from 1 June to tuesday"
-response = parse_request(input_text)
-print(response)
-
-input_text = "Schedule meeting from today to tomorrow"
-response = parse_request(input_text)
-print(response)
-
-input_text = "Schedule meeting from yesterday to tomorrow"
-response = parse_request(input_text)
-print(response)
-
-input_text = "Schedule meeting from today to monday"
-response = parse_request(input_text)
-print(response)
+for input_text in test_cases:
+    response = parse_request(input_text)
+    print(response)
 
 
